5b5adcf5d712a89776420c86ad6a580d1980e83b
kawakubo <kawakubo.hidetoshi@meta.co.jp>
2022-02-16 17:29:36 +0900

    Add RealtimeOcrActivity

--- app/src/main/java/com/mayoigacraft/phyrexia/MainActivity.kt
+++ app/src/main/java/com/mayoigacraft/phyrexia/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.mayoigacraft.phyrexia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,9 +12,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onStartButtonClicked(view: View) {
-        toast = Toast.makeText(this, "Hello", Toast.LENGTH_SHORT)
-        toast?.show()
+        val intent = Intent(applicationContext, RealtimeOcrActivity::class.java)
+        startActivity(intent)
     }
-
-    private var toast:Toast? = null
 }
