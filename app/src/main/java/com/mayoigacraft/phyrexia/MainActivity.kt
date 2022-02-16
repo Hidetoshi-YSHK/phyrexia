8629b900d3423242eadf98284683e341995b37bf
kawakubo <kawakubo.hidetoshi@meta.co.jp>
2022-02-16 17:17:17 +0900

    Add onStartButtonClicked

--- app/src/main/java/com/mayoigacraft/phyrexia/MainActivity.kt
+++ app/src/main/java/com/mayoigacraft/phyrexia/MainActivity.kt
@@ -2,10 +2,19 @@ package com.mayoigacraft.phyrexia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun onStartButtonClicked(view: View) {
+        toast = Toast.makeText(this, "Hello", Toast.LENGTH_SHORT)
+        toast?.show()
+    }
+
+    private var toast:Toast? = null
 }
