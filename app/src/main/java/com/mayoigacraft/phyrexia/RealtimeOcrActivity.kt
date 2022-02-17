e58cf1cc86c44b786200c8538ce2f64f9753b906
kawakubo <kawakubo.hidetoshi@meta.co.jp>
2022-02-17 12:52:59 +0900

    Add a preview feature and a analysis feature

--- app/src/main/java/com/mayoigacraft/phyrexia/RealtimeOcrActivity.kt
+++ app/src/main/java/com/mayoigacraft/phyrexia/RealtimeOcrActivity.kt
@@ -5,12 +5,21 @@ import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.mayoigacraft.phyrexia.databinding.ActivityRealtimeOcrBinding
+import java.nio.ByteBuffer
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
+
+typealias LumaListener = (luma: Double) -> Unit
 
 class RealtimeOcrActivity : AppCompatActivity() {
 
@@ -54,6 +63,7 @@ class RealtimeOcrActivity : AppCompatActivity() {
     }
 
     companion object {
+        private const val APP_NAME = "Phyrexia"
         private const val REQUEST_CODE_PERMISSIONS = 10
         private val REQUIRED_PERMISSIONS =
             mutableListOf (
@@ -66,7 +76,52 @@ class RealtimeOcrActivity : AppCompatActivity() {
             }.toTypedArray()
     }
 
-    private fun startCamera() {}
+    private fun startCamera() {
+        val cameraProviderFuture =
+            ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider =
+                cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(
+                        viewBinding.cameraPreview.surfaceProvider)
+                }
+
+            // Analyzer
+            val imageAnalyzer = ImageAnalysis.Builder()
+                .build()
+                .also {
+                    it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
+                        Log.d(APP_NAME, "Average luminosity: $luma")
+                    })
+                }
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this,
+                    cameraSelector,
+                    preview,
+                    imageAnalyzer)
+
+            } catch(e: Exception) {
+                Log.e(APP_NAME, "Use case binding failed", e)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+    }
 
     private fun isAllPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         val permissionState = ContextCompat.checkSelfPermission(baseContext, it)
@@ -79,4 +134,26 @@ class RealtimeOcrActivity : AppCompatActivity() {
     // Executor of camera
     private lateinit var cameraExecutor: ExecutorService
 
+    private class LuminosityAnalyzer(
+        private val listener: LumaListener) : ImageAnalysis.Analyzer {
+
+        private fun ByteBuffer.toByteArray(): ByteArray {
+            rewind()    // Rewind the buffer to zero
+            val data = ByteArray(remaining())
+            get(data)   // Copy the buffer into a byte array
+            return data // Return the byte array
+        }
+
+        override fun analyze(image: ImageProxy) {
+
+            val buffer = image.planes[0].buffer
+            val data = buffer.toByteArray()
+            val pixels = data.map { it.toInt() and 0xFF }
+            val luma = pixels.average()
+
+            listener(luma)
+
+            image.close()
+        }
+    }
 }
