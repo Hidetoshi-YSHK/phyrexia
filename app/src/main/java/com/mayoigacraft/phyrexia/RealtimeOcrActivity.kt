2074d76549494b31e59baa15f1f8efff1a879c65
kawakubo <kawakubo.hidetoshi@meta.co.jp>
2022-02-17 11:22:03 +0900

    Add a preview view

--- app/src/main/java/com/mayoigacraft/phyrexia/RealtimeOcrActivity.kt
+++ app/src/main/java/com/mayoigacraft/phyrexia/RealtimeOcrActivity.kt
@@ -2,10 +2,16 @@ package com.mayoigacraft.phyrexia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mayoigacraft.phyrexia.databinding.ActivityRealtimeOcrBinding
 
 class RealtimeOcrActivity : AppCompatActivity() {
+
+    // View binding
+    private lateinit var viewBinding: ActivityRealtimeOcrBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_realtime_ocr_activiry)
+        viewBinding = ActivityRealtimeOcrBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
     }
 }
