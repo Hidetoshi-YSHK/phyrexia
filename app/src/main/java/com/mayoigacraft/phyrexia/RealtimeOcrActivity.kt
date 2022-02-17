05eaf234135de60216769b5da727f22edb6d5798
kawakubo <kawakubo.hidetoshi@meta.co.jp>
2022-02-17 11:46:52 +0900

    Add a request of permission

--- app/src/main/java/com/mayoigacraft/phyrexia/RealtimeOcrActivity.kt
+++ app/src/main/java/com/mayoigacraft/phyrexia/RealtimeOcrActivity.kt
@@ -1,17 +1,64 @@
 package com.mayoigacraft.phyrexia
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.mayoigacraft.phyrexia.databinding.ActivityRealtimeOcrBinding
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class RealtimeOcrActivity : AppCompatActivity() {
-
-    // View binding
-    private lateinit var viewBinding: ActivityRealtimeOcrBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityRealtimeOcrBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
+
+        if (isAllPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS)
+        }
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    companion object {
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf (
+                Manifest.permission.CAMERA
+            ).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    add(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
+            }.toTypedArray()
+    }
+
+    private fun startCamera() {}
+
+    private fun isAllPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        val permissionState = ContextCompat.checkSelfPermission(baseContext, it)
+        (permissionState == PackageManager.PERMISSION_GRANTED)
+    }
+
+    // View binding
+    private lateinit var viewBinding: ActivityRealtimeOcrBinding
+
+    // Executor of camera
+    private lateinit var cameraExecutor: ExecutorService
+
 }
