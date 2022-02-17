ab1d429c88c93d5ffdafb44f5b59b556f69135e6
kawakubo <kawakubo.hidetoshi@meta.co.jp>
2022-02-17 17:09:12 +0900

    Add hidden configs

--- app/src/main/java/com/mayoigacraft/phyrexia/RealtimeOcrActivity.kt
+++ app/src/main/java/com/mayoigacraft/phyrexia/RealtimeOcrActivity.kt
@@ -3,11 +3,15 @@ package com.mayoigacraft.phyrexia
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import android.hardware.camera2.CaptureRequest
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.util.Size
 import android.widget.Toast
+import androidx.camera.camera2.impl.Camera2ImplConfig
+import androidx.camera.camera2.internal.Camera2CameraControlImpl
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
@@ -16,6 +20,7 @@ import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.Text
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.TextRecognizer
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions
@@ -23,7 +28,7 @@ import com.mayoigacraft.phyrexia.databinding.ActivityRealtimeOcrBinding
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
-typealias OcrListener = (text: String) -> Unit
+typealias OcrListener = (textInfo: Text) -> Unit
 
 class RealtimeOcrActivity : AppCompatActivity() {
 
@@ -47,6 +52,7 @@ class RealtimeOcrActivity : AppCompatActivity() {
         textRecognizer = TextRecognition.getClient(
             TextRecognizerOptions.DEFAULT_OPTIONS
         )
+
     }
 
     override fun onDestroy() {
@@ -88,6 +94,7 @@ class RealtimeOcrActivity : AppCompatActivity() {
             }.toTypedArray()
     }
 
+    @SuppressLint("RestrictedApi", "UnsafeOptInUsageError")
     private fun startCamera() {
         val cameraProviderFuture =
             ProcessCameraProvider.getInstance(this)
@@ -96,6 +103,45 @@ class RealtimeOcrActivity : AppCompatActivity() {
             // Used to bind the lifecycle of cameras to the lifecycle owner
             val cameraProvider: ProcessCameraProvider =
                 cameraProviderFuture.get()
+
+
+            // Cameraの細かい設定を非公開APIで設定する
+            var useHiddenConfigFlag = false
+            val configBuilder = Camera2ImplConfig.Builder()
+            if (useHiddenConfigFlag) {
+                configBuilder.setCaptureRequestOption(
+                    CaptureRequest.CONTROL_MODE,
+                    CaptureRequest.CONTROL_MODE_AUTO
+                )
+                configBuilder.setCaptureRequestOption(
+                    CaptureRequest.CONTROL_AE_MODE,
+                    CaptureRequest.CONTROL_AE_MODE_ON
+                )
+                configBuilder.setCaptureRequestOption(
+                    CaptureRequest.CONTROL_AF_MODE,
+                    CaptureRequest.CONTROL_AF_MODE_AUTO
+                )
+                configBuilder.setCaptureRequestOption(
+                    CaptureRequest.CONTROL_AWB_MODE,
+                    CaptureRequest.CONTROL_AWB_MODE_AUTO
+                )
+                configBuilder.setCaptureRequestOption(
+                    CaptureRequest.FLASH_MODE,
+                    CaptureRequest.FLASH_MODE_OFF
+                )
+                configBuilder.setCaptureRequestOption(
+                    CaptureRequest.SENSOR_SENSITIVITY,
+                    100
+                )
+                configBuilder.setCaptureRequestOption(
+                    CaptureRequest.SENSOR_FRAME_DURATION,
+                    16666666
+                )
+                configBuilder.setCaptureRequestOption(
+                    CaptureRequest.SENSOR_EXPOSURE_TIME,
+                    20400000
+                )
+            }
 
             // Preview
             val preview = Preview.Builder()
@@ -110,6 +156,7 @@ class RealtimeOcrActivity : AppCompatActivity() {
             val imageAnalyzer = ImageAnalysis.Builder()
                 .setBackpressureStrategy(
                     ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .setTargetResolution(Size(640, 480))
                 .build()
                 .also {
                     it.setAnalyzer(
@@ -125,12 +172,17 @@ class RealtimeOcrActivity : AppCompatActivity() {
                 cameraProvider.unbindAll()
 
                 // Bind use cases to camera
-                cameraProvider.bindToLifecycle(
+                var camera = cameraProvider.bindToLifecycle(
                     this,
                     cameraSelector,
                     preview,
                     imageAnalyzer
                 )
+
+                if (useHiddenConfigFlag) {
+                    (camera.cameraControl as Camera2CameraControlImpl)
+                        .addInteropConfig(configBuilder.build())
+                }
 
             } catch (e: Exception) {
                 Log.e(APP_NAME, "Use case binding failed", e)
@@ -144,8 +196,8 @@ class RealtimeOcrActivity : AppCompatActivity() {
         (permissionState == PackageManager.PERMISSION_GRANTED)
     }
 
-    private fun onOcrSucceeded(text : String) {
-        Log.e(APP_NAME, text)
+    private fun onOcrSucceeded(text : Text) {
+        Log.e(APP_NAME, text.text)
     }
 
     private lateinit var viewBinding: ActivityRealtimeOcrBinding
@@ -168,11 +220,12 @@ class RealtimeOcrActivity : AppCompatActivity() {
                     imageProxy.imageInfo.rotationDegrees)
 
                 val result = textRecognizer.process(inputImage)
-                    .addOnSuccessListener { visionText ->
-                        listener(visionText.text)
+                    .addOnSuccessListener { textInfo ->
+                        listener(textInfo)
                         imageProxy.close()
                     }
                     .addOnFailureListener { e ->
+                        Log.e(APP_NAME, "Text recognition failed", e)
                         imageProxy.close()
                     }
             }
