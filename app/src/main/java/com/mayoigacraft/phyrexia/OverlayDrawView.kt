34e2cea611d85ec539e9079f9c77c3493cd533b0
kawakubo <kawakubo.hidetoshi@meta.co.jp>
2022-02-22 13:12:17 +0900

    Add PercentRect.kt

--- app/src/main/java/com/mayoigacraft/phyrexia/OverlayDrawView.kt
+++ app/src/main/java/com/mayoigacraft/phyrexia/OverlayDrawView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.PorterDuff
 import android.util.AttributeSet
 import android.view.View
 
@@ -15,20 +16,74 @@ class OverlayDrawView(
     attributeSet: AttributeSet?
 ) : View(context, attributeSet) {
 
-    private var paint: Paint = Paint()
+    companion object {
+        const val LINE_WIDTH : Float = 10f
+        private const val LINE_OPACITY = 255
+        private val LINE_COLOR_RED = Color.argb(
+            LINE_OPACITY,
+            211,
+            47,
+            47
+        )
+        private val LINE_COLOR_PURPLE = Color.argb(
+            LINE_OPACITY,
+            123,
+            31,
+            162
+        )
+        private val LINE_COLOR_INDIGO = Color.argb(
+            LINE_OPACITY,
+            48,
+            63,
+            159
+        )
+        private val LINE_COLOR_LIGHT_BLUE = Color.argb(
+            LINE_OPACITY,
+            2,
+            136,
+            209
+        )
+        private val LINE_COLOR_TEAL = Color.argb(
+            LINE_OPACITY,
+            0,
+            121,
+            107
+        )
+        private val LINE_COLOR_LIGHT_GREEN = Color.argb(
+            LINE_OPACITY,
+            104,
+            159,
+            56
+        )
+        private val LINE_COLOR_LIME = Color.argb(
+            LINE_OPACITY,
+            175,
+            180,
+            43
+        )
+        val LINE_COLORS = arrayListOf<Int>(
+            LINE_COLOR_RED,
+            LINE_COLOR_PURPLE,
+            LINE_COLOR_INDIGO,
+            LINE_COLOR_LIGHT_BLUE,
+            LINE_COLOR_TEAL,
+            LINE_COLOR_LIGHT_GREEN,
+            LINE_COLOR_LIME
+        )
 
-    // 描画するラインの太さ
-    private val lineStrokeWidth = 20f
+    }
 
     init {}
 
     override fun onDraw(canvas: Canvas) {
+        // キャンバスをクリア
+        canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR)
 
         // ペイントする色の設定
         paint.color = Color.argb(255, 255, 0, 255)
 
         // ペイントストロークの太さを設定
-        paint.strokeWidth = lineStrokeWidth
+        paint.strokeWidth = LINE_WIDTH
 
         // Styleのストロークを設定する
         paint.style = Paint.Style.STROKE
@@ -37,4 +92,6 @@ class OverlayDrawView(
         // (x1,y1,x2,y2,paint) 左上の座標(x1,y1), 右下の座標(x2,y2)
         canvas.drawRect(300f, 300f, 600f, 600f, paint)
     }
+
+    private var paint: Paint = Paint()
 }
---
+++ app/src/main/java/com/mayoigacraft/phyrexia/PercentRect.kt
@@ -0,0 +1,8 @@
+package com.mayoigacraft.phyrexia
+
+data class PercentRect(
+    var top : Float,
+    var left : Float,
+    var bottom : Float,
+    var right : Float
+)
